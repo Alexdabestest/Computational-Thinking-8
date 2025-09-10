7c5188d945b82ccd48fa095fd0cd38e0a9910068
Alexdabestest <alexnielsen@seattleacademy.org>
2025-09-10 18:28:04 +0000

    Hello Josh, this is Alex.

--- Projects/welcome_to_ct8.py
+++ Projects/welcome_to_ct8.py
@@ -26,5 +26,5 @@ print("\n\n")
                                                                                           
                                                                                           
                                   
-print("Now it's your turn:")
-print("Find this line (line 30) in the welcome_to_ct8.py file, then change the message to say a fact about you.")                                  
+print("One fact about me is that" )
+print("I like reading books")                                  
