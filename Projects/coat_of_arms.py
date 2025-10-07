7e3153e070e3311ce336368b891e0a2562ce7e1a
Alexdabestest <alexnielsen@seattleacademy.org>
2025-10-07 21:37:46 +0000

    Im done.

---
+++ Projects/coat_of_arms.py
@@ -0,0 +1,86 @@
+# Section 1 - Helper functions (DON'T CHANGE!!)
+import turtle, math, time, random
+def set_background(image_filename):
+	screen = turtle.Screen()
+	try:
+		screen.bgpic(f"/workspaces/Computational-Thinking-8/Backgrounds/{image_filename}.png")
+	except:
+		screen.bgpic(f"/workspaces/Computational-Thinking-8/Backgrounds/{image_filename}.gif")
+
+def set_image(sprite, image_filename):
+    image_file = f"./Images/{image_filename}.gif"
+    screen = turtle.Screen()
+    screen.register_shape(image_file)
+    sprite.shape(image_file)
+
+def create_sprite(image_filename, x=0, y=0):
+    sprite = turtle.Turtle()
+    set_image(sprite, image_filename)
+    sprite.penup()
+    sprite.goto(x,y)
+    window.update()
+    return sprite
+
+def get_distance(s1, s2):
+	dx = s1.xcor() - s2.xcor()
+	dy = s1.ycor() - s2.ycor()
+	return math.sqrt(dx*dx + dy*dy)
+
+def draw_rectangle( color="black",x=0,y=0, width=100, height=100,):
+	sprite = turtle.Turtle()
+	sprite.speed(0)
+	sprite.pencolor(color)
+	sprite.color(color)
+	sprite.penup()
+	sprite.goto(x - (width*0.5), y + (height*0.5))
+	sprite.pendown()
+	sprite.begin_fill()
+	for i in range(2):
+		sprite.forward(width)
+		sprite.right(90)
+		sprite.forward(height)
+		sprite.right(90)
+	sprite.end_fill()
+	sprite.hideturtle()
+
+
+window = turtle.Screen()
+window.tracer(0)
+
+
+
+######################################################################
+# https://en.wikipedia.org/wiki/Web_colors#Extended_colors
+# Section 2 - Your code
+set_background("fall")
+
+#these are my rectangles
+draw_rectangle("blue", 100, 100, 200, 200)
+draw_rectangle("purple", -100, 100, 200, 200)
+draw_rectangle("white", 100, -100, 200, 200)
+draw_rectangle("red", -100, -100, 200, 200)
+
+#these are my sprites
+s1 = create_sprite("Books", 100, 100)
+s2 = create_sprite("paper_cat", -70, -100)
+s3 = create_sprite("yarn_bluee", 100, -100)
+s4 = create_sprite("pencil", -100, 100)
+
+#these are the words
+message1 = create_sprite("alien",-150,200)
+message1.color("black")
+message1.write("Alex Nielsen",font = ("Arial", 40, "normal"))
+message1.hideturtle()
+
+message2 = create_sprite("alien",-150,-260)
+message2.color("black")
+message2.write("Alex Nielsen",font = ("Arial", 40, "normal"))
+message2.hideturtle()
+
+
+######################################################################
+
+
+# Section 3 - Keeping the window open (DON'T CHANGE!!)
+window.update()
+turtle.exitonclick()
